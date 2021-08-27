1a83601b97f0c45d627720fbadd57438cc97884e
Victor Umunna <umunnavictor01@gmail.com>
2021-08-27 23:15:35 +0100

    update

--- oop.py
+++ oop.py
@@ -1,7 +1,7 @@
 
 class Employee:
     num_of_emps = 0
-    raise_amount = 1.04
+    raise_amt = 1.04
     
 
     def __init__(self, first, last, pay):
@@ -17,10 +17,24 @@ class Employee:
         return f'{self.first} {self.last}'
 
     def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amount)
+        self.pay = int(self.pay * self.raise_amt)
 
-emp_1 = Employee('Victor', 'Umunna',50000)
-emp_2 = Employee('Test', 'User', 60000)
+    def __repr__(self):
+        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
 
+    def __str__(self):
+        return f'{self.fullname()} - {self.email}'
+    
+    def __add__(self,other):
+        return self.pay + other.pay
 
-print(Employee.num_of_emps)
+    def __len__(self):
+         return  len(self.fullname())  
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'Employee', 60000)
+
+print(len(emp_1))
+
+# print(repr(emp_1))
+# print(str(emp_1))
